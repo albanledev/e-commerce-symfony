30b644c934eeb14c392a9651ce9639e8a57430a4
Alban de Braquilanges <alban2bqs@gmail.com>
2023-02-19 12:30:41 +0100

    :zap: ajout form de tri

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,23 +2,28 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\Admin\UserCrudController;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        # return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+         return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -40,7 +45,11 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-poo', Product::class);
+
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,18 +2,68 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository ): Response
     {
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+            //'controller_name' => 'AppController',
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
+
+    #[Route('/category/{id}', name: 'app_category')]
+    public function category($id, CategoryRepository $categoryRepository, ProductRepository $productRepository, Request $request): Response
+    {
+        $category = $categoryRepository->find($id);
+
+        if(!$category){
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        $sort = $request->query->get('sort');
+
+        if ($sort == 'price_asc') {
+            $products = $productRepository->findBy(['category' => $category], ['price' => 'ASC']);
+        } else if ($sort == 'price_desc') {
+            $products = $productRepository->findBy(['category' => $category], ['price' => 'DESC']);
+        } else {
+            $products = $category->getProducts();
+        }
+
+        return $this->render('app/category.html.twig', [
+            'category' => $category,
+            'products' => $products,
+            'sort' => $sort,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_product')]
+    public function product($id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+        #dd($district);
+
+        if(!$product){
+            throw $this->createNotFoundException('Product not founddd');
+        }
+
+
+        return $this->render('app/product.html.twig', [
+            #'controller_name' => 'McdoController',
+            'product' => $product,
+        ]);
+    }
+
+
 
 }
